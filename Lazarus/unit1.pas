027f9839abd4a0d7669b37ecbdbc0830ded2a315
andreas <Andreas-Filsinger@users.noreply.github.com>
2014-01-21 10:02:45 +0000

    Lazarus: Erste Versuche mit Farben

--- Lazarus/unit1.pas
+++ Lazarus/unit1.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, IBConnection, sqldb, DB, FileUtil, Forms, Controls,
-  Graphics, Dialogs, DBGrids, StdCtrls, DBCtrls, ZConnection, ZDataset;
+  Graphics, Dialogs, DBGrids, StdCtrls, DBCtrls, ZConnection, ZDataset,
+  ZSqlUpdate;
 
 type
 
@@ -23,10 +24,16 @@ type
     SQLTransaction1: TSQLTransaction;
     ZConnection1: TZConnection;
     ZQuery1: TZQuery;
+    procedure Datasource1StateChange(Sender: TObject);
     procedure DBMemo1Change(Sender: TObject);
     procedure FormActivate(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
+    procedure ZQuery1AfterEdit(DataSet: TDataSet);
+    procedure ZQuery1AfterPost(DataSet: TDataSet);
+    procedure ZQuery1ApplyUpdateError(DataSet: TDataSet; E: EDatabaseError;
+      var DataAction: TDataAction);
+    procedure ZQuery1BeforeEdit(DataSet: TDataSet);
   private
     { private declarations }
   public
@@ -77,9 +84,45 @@ begin
 
 end;
 
+procedure TForm1.ZQuery1AfterEdit(DataSet: TDataSet);
+begin
+
+end;
+
+procedure TForm1.ZQuery1AfterPost(DataSet: TDataSet);
+begin
+
+end;
+
+procedure TForm1.ZQuery1ApplyUpdateError(DataSet: TDataSet; E: EDatabaseError;
+  var DataAction: TDataAction);
+begin
+
+end;
+
+procedure TForm1.ZQuery1BeforeEdit(DataSet: TDataSet);
+begin
+
+end;
+
 procedure TForm1.DBMemo1Change(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.Datasource1StateChange(Sender: TObject);
+var
+    s: Tdatasetstate;
+begin
+  {   dsInactive, dsBrowse, , dsInsert, dsSetKey,
+      dsCalcFields, dsFilter, dsNewValue, dsOldValue, dsCurValue, dsBlockRead,
+      dsInternalCalc, dsOpening
+}
+  case (Sender as TDatasource).State of
+   dsInsert: Memo1.Color:=cllime;
+   dsEdit: Memo1.Color:=clyellow;
+   dsBrowse:Memo1.Color:=clwindow;
+  end;
 end;
 
 end.
