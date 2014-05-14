497edfd017d0997a855346a2d5b4a454520e3bf1
andreas <andreas@a29c809c-81a6-11e3-82df-89085422e4c6>
2014-05-14 09:40:18 +0000

    OrgaMon Rev 8.023

--- OrgaMon/globals.pas
+++ OrgaMon/globals.pas
@@ -45,7 +45,7 @@ uses
 
 const
   cApplicationName = 'OrgaMon'; // CRYPT-KEY! - never Change a bit!!!
-  Version: single = 8.022; // ..\rev\OrgaMon.rev.txt
+  Version: single = 8.023; // ..\rev\OrgaMon.rev.txt
   cVersion_JonDa: single = 1.118;
   cVersion_OrgaMonApp: single = 2.000;
 
