f8ca48d2c148b9d2f1ddee0a6a2d911930a0074e
andreas <Andreas-Filsinger@users.noreply.github.com>
2016-01-07 10:26:24 +0000

    Templates Anpassungen

---
+++ TWebShop/templates/hebu2008musikverlag/const.php
@@ -0,0 +1,5 @@
+﻿<?php
+
+include_once("../templates/hebu2008music/const.php");
+
+define("__TEMPLATE_PROJECTNAME","TemplateHeBu2008Musikverlag");
---
+++ TWebShop/templates/hebu2008streicher/const.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once("../templates/hebu2008music/const.php");
+
+define("__TEMPLATE_PROJECTNAME","TemplateHeBu2008Streicher");
