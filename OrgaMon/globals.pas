e32c2267e16d70f20d5e43137468c6cbe15e118f
andreas <andreas@a29c809c-81a6-11e3-82df-89085422e4c6>
2016-02-10 21:11:05 +0000

    Foto-Service

--- OrgaMon/globals.pas
+++ OrgaMon/globals.pas
@@ -47,7 +47,7 @@ uses
 
 const
   cApplicationName = 'OrgaMon'; // CRYPT-KEY! - never Change a bit!!!
-  Version: single = 8.150; // ..\rev\OrgaMon.rev.txt
+  Version: single = 8.151; // ..\rev\OrgaMon.rev.txt
 
   // Mindest-Anforderungen
   cMinVersion_OrgaMonApp: single = 2.020;
